b04cbb5b87758c8f68d0984872bd328d553a80d6
luvsugo-afk <ailencortez014@gmail.com>
2026-08-18 22:44:19 -0300

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 // CORS
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, x-api-key');
     if (req.method === 'OPTIONS') {
         return res.sendStatus(200);
